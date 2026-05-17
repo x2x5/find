bdb253647d5a27885c48f4573f1e2329b2252304
x2x5 <sleeptian@hotmail.com>
2026-05-17 16:55:24 +0800

    Revert: keep default to ML+CV conferences only

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -15,7 +15,7 @@ function AppContent() {
   const defaultYear = new Date().getFullYear();
 
   const [selectedConfs, setSelectedConfs] = useState<Set<string>>(
-    () => new Set(['nips', 'icml', 'iclr', 'cvpr', 'eccv', 'iccv', 'aaai', 'ijcai', 'mm'])
+    () => new Set(['nips', 'icml', 'iclr', 'cvpr', 'eccv', 'iccv'])
   );
   const [yearRange, setYearRange] = useState<[number, number]>([defaultYear - 1, defaultYear]);
   const [searchValue, setSearchValue] = useState('');
